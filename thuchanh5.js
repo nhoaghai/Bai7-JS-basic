712f078e7b2f635d5a3372f9d2004dee96489a0d
nhoaghai <117527074+nhoaghai@users.noreply.github.com>
2023-02-01 20:02:08 +0700

    Add files via upload

---
+++ thuchanh5.js
@@ -0,0 +1,12 @@
+let inputWidth;
+let inputHeight;
+
+inputWidth = prompt('Enter the Width');
+inputHeight = prompt('Enter the Height');
+
+let Width = parseInt(inputWidth);
+let Height = parseInt(inputHeight);
+
+let area = Width * Height
+
+alert('Area =' + area);
